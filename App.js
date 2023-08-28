611f679f6fe406a310af65e114e2b7d08ee0beee
Douglaswvsilva <142617045+Douglaswvsilva@users.noreply.github.com>
2023-08-27 22:43:29 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import logo from "./img/assustado.png";
+import macaco from "./img/beijo.png"
+import beijo from "./img/macaco.png"
+import rindo from "./img/rindo.png"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <img src={logo} title='assustado'height='500px' width='500px'/>
+      <p>Clique no emoji para exibir o nome</p>
+      <img src={macaco} title='beijo' height='100px' width='100px' />
+      <img src={beijo} title='Macaco' height='100px' width='100px' />
+      <img src={rindo} title='rindo' height='100px' width='100px' />
+    </div>
+  );
+}
+
+export default App;
